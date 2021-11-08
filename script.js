54795060fe0fee08c867d5ebbb978739c400795d
Kukuynik <ilya42rs@gmail.com>
2021-11-08 12:47:19 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var Name = "Это лабораторная работа по JavaScript";
+alert(Name);
+console.log(Name);
+
+var a = 25;
+var b = 13;
+alert("Переменная a ="+a+"переменная b ="+b);
+console.log("Переменная a ="+a+" переменная b ="+b);
+var c = a+b;
+alert("Сумма a+b = "+c);
+console.log("Сумма a+b = "+c);
